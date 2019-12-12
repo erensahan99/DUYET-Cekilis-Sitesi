92e6622abd21a453d58d3d93ef72cebe7f52d3e0
Eren <eren.sahan99@gmail.com>
2019-12-12 19:32:20 +0300

    table changed cekilis-ayrinti.html

--- dbconnect.py
+++ dbconnect.py
@@ -7,16 +7,17 @@ def connection():
                          db="heroku_f580b65af1fa5c6",
                          autocommit=True,
                          charset='utf8')
+
     c=conn.cursor()
     c.execute("SET NAMES UTF8")
     c.execute("SET CHARACTER SET utf8")
     c.execute("SET COLLATION_CONNECTION = 'utf8_turkish_ci'")
     return c,conn
 '''
-    conn=MySQLdb.connect(host="localhost",
-                         user= "root",
-                         passwd="",
-                         db="duyet",
-                         autocommit=True,
-                         charset='utf8')
+conn=MySQLdb.connect(host="localhost",
+                     user= "root",
+                     passwd="",
+                     db="duyet",
+                     autocommit=True,
+                     charset='utf8')
 '''
